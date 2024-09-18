365738dd0d90eac3458c3d5702f1010e5b9a2cad
tom2811 <oaksoe.thein01@gmail.com>
2024-09-18 15:59:56 +0630

    Fix some errors

--- parser.py
+++ parser.py
@@ -1,3 +1,4 @@
+import os
 import re
 from typing import Dict, List, Optional, Tuple
 from urllib.parse import urljoin
@@ -267,4 +268,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
